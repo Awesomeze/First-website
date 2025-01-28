526e2d0a246cfdeb91a9f7ceb00f51dcbfe00804
Eze <ezeawesomeeze@gmail.com>
2025-01-28 14:03:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.menu-btn')
+const navList = document.querySelector('.navlist')
+
+menuBtn.addEventListener('click',function(){
+    navList.classList.toggle('active')
+})
+
+
+
+
+
